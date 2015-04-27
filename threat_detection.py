a056502fa30479d4621c055c629b6803373f5cc9
root <root@thought2.fcl.wp.comcast.net>
2015-04-26 19:35:52 -0500

    Encoding Issue fixed: Use ISO-8859-1 encodig instead of 'UTF-8' or 'ASCII'

--- threat_detection.py
+++ threat_detection.py
@@ -5,11 +5,21 @@ Threat Detection using tcpdump
 '''
 # In-built python packages
 import os, time, sys, getopt
+'''
+Strange: collections does not have the module - 'OrderedDict' in python 2.6.9
+Use ordereddict instead
+'''
+'''
 if sys.version_info >= (2, 7):
     import collections
 else:
     import ordereddict as collections
-
+'''
+import ordereddict
+import sys
+reload(sys)
+#sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('iso-8859-1')
 # Traffic capture packages
 import dpkt
 #from scapy.all import sr1,IP,ICMP,rdpcap
@@ -21,9 +31,6 @@ import uuid #for generating uuids
 
 
 
-#TODO add support for providing more options to tcpudump
-def capture_traffic(traffic_cap):
-  os.system("tcpdump -nS -c10000 -w {traffic_cap}")  
 
 # TODO : Pass pcap file as input
 def print_packet_stats():
@@ -56,8 +63,14 @@ def count_packets(traffic_cap):
 def convert_traffic(traffic_cap):
   #raw_data = rdpcap("traffic")
   #print raw_data
-  traffic_data = list((p[IP].src, p[IP].dst, p[IP].payload) for p in PcapReader(traffic_cap) if IP in p)
+  traffic_data = list((p[IP].src, p[IP].dst, (str(p[IP].payload)).encode('utf-8').strip()) for p in PcapReader(traffic_cap) if IP in p)
   return traffic_data
+
+
+#TODO add support for providing more options to tcpudump
+def capture_traffic(traffic_cap):
+  cmd = "tcpdump -nS -c100 -w " + traffic_cap 
+  os.system(cmd)  
 
 # TODO: make gpudb_ip an argument
 def add_data(traffic_data):
@@ -71,7 +84,7 @@ def add_data(traffic_data):
                              {"name":"x","type":"double"},
                              {"name":"y","type":"double"},
                              {"name":"src","type":"string"},
-                             {"name":"dst","type":"string"}
+                             {"name":"dst","type":"string"},
                              {"name":"payload","type":"string"}
                           ]
                        }"""
@@ -85,18 +98,18 @@ def add_data(traffic_data):
   x = 1;y = 1 
   encoded_datums = []
   for e in traffic_data: 
-    datum = collections.OrderedDict([('x',x), ('y',y), ('src',e[0]),('dst',e[1]),('payload',e[2])])
+    datum = ordereddict.OrderedDict([('x',x), ('y',y), ('src',e[0]),('dst',e[1]),('payload',e[2])])
     encoded_datum = gpudb.encode_datum(type_definition,datum)
     encoded_datums.append(encoded_datum)
     x+=1;y+=1
      
   gpudb.do_bulk_add(set_id, encoded_datums)
 
-  return set_id
+  return set_id,gpudb
   
 # Query GPUdb
 # TODO: define different machine learning queries
-def query(set_id):
+def query(set_id, gpudb):
   print "Querying ..."
   lower_bound = 1; upper_bound = 1
   attribute_key = "x"
@@ -131,7 +144,7 @@ def main(argv):
     print traffic_cap + " not present. Capturing Traffic ..."
     # Capture Packets in a pcap file.
     capture_traffic(traffic_cap)
-    print 'Traffic Captured in the file: "', traffic_cap
+    print 'Traffic Captured in the file: ', traffic_cap
     # Count packets 
     # count_packets(traffic_cap)
 
@@ -143,15 +156,24 @@ def main(argv):
   traffic_data = convert_traffic(traffic_cap)
 
   # TODO: Add Data to GPUdb
-  set_id = add_data(traffic_data)
+  set_id, gpudb = add_data(traffic_data)
   print "Data stored at GPUdb"
 
   # Query GPUdb server
-  query(set_id)
+  query(set_id, gpudb)
  
   # TODO: Record Stats of the query
   print 'Sats Recorded in the file: "', stats
 
 
+#TODO: make packet capture real time
+# callback function - called for every packet
+def traffic_monitor_callbak(pkt):
+    if IP in pkt:
+        print pkt.sprintf("%IP.len%")
+
+# capture traffic for 10 seconds
+def capture_traffic_rt(traffic_cap):
+  sniff(iface="eth0", prn=traffic_monitor_callbak, store=0, timeout=10)
 if __name__ == "__main__":
    main(sys.argv[1:])
